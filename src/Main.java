44947fab2712486cbdc73a4e28be1becdb5fdcf5
Sapargul <kosalievasapargul197@gmail.com>
2026-02-19 10:10:25 +0600

    Added new subject Java

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Student: Ali");
         System.out.println("Sapargul");
+        System.out.println("subject -"+"Java");
     }
 }
