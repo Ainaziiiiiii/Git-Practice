80603ca42c344cf346f8f545c19b1e7aec2fb447
Sapargul <kosalievasapargul197@gmail.com>
2026-02-19 12:05:17 +0600

    add models

--- src/CounsumerMethods.java
+++
@@ -1,5 +0,0 @@
-import model.Consumer;
-
-public interface CounsumerMethods {
-    Consumer c
-}
--- src/model/Consumer.java
+++ src/model/Consumer.java
@@ -6,5 +6,54 @@ public class Consumer {
     private long id;
     private String name;
     private String email;
-    private
+    private List<Order> orders;
+
+    public Consumer(long id, String name, String email, List<Order> orders) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.orders = orders;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public String toString() {
+        return "Consumer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", orders=" + orders +
+                '}';
+    }
 }
