5649ca1c5ecf913391ebe322865567e5c04dfe23
Даниэл <danielmuktarbekov99@gmail.com>
2026-02-19 11:21:36 +0600

    added models product

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Student: Ali");
-        System.out.println("Ainazik");
-        System.out.println("Subject -" + "Java");
+
+
+
+
     }
 }
