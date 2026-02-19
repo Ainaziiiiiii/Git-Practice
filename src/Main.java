dae387913178360a3dfea68dad7e3e90ce7f8f4e
Timur <kyrgyibekovtimur@gmail.com>
2026-02-19 10:09:48 +0600

    Added new subject Java

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Student: Ali");
         System.out.println("Timur");
+        System.out.println("Subject -"+"Java");
     }
 }
