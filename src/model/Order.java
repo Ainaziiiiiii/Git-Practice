04a5a83e31ba2e79541c4f675d53a10ab9e28640
user <rashidadilbekuluu79@gmil.com>
2026-02-19 11:32:34 +0600

    Added model/class/order/orderItems

--- src/model/Order.java
+++ src/model/Order.java
@@ -2,7 +2,52 @@ package model;
 
 public class Order {
     private Long id;
-    private Customer customer;
     private String items;
     private String status;
+
+    public Order() {
+    }
+
+    public Order(Long id, String items, String status) {
+        this.id = id;
+        this.items = items;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItems() {
+        return items;
+    }
+
+    public void setItems(String items) {
+        this.items = items;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void addItems(OrderItem item) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", items='" + items + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
