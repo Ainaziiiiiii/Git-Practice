6bab6d47642017f6872807482bbd85c51f4c7e43
Даниэл <danielmuktarbekov99@gmail.com>
2026-02-19 14:48:02 +0600

    added models product

---
+++ src/db/Database.java
@@ -0,0 +1,12 @@
+package db;
+
+import model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+   public static List<Product> products=new ArrayList<>();
+
+
+}
