78feda0b4ac50827bcf4b8380633ad71d854bf61
Almazbek <almazbek@gmail.com>
2026-02-19 12:13:37 +0100

    Added new OrderService

---
+++ src/OrderService.java
@@ -0,0 +1,17 @@
+import dao.OrderDao;
+import model.Customer;
+import model.Order;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderService {
+
+
+    void createOrder (Customer c);
+    void addItemToOrder (int orderId, int productId, int  qty);
+    void calculateTotal(int orderId);
+    void cancelOrder(int orderId);
+    List<Order> getOrderByStatus (String status);
+
+}
